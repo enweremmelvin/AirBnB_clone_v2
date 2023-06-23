2fd89819339060b7dc37354a75eaa3865e7a74c3
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-06-23 14:07:43 +0000

    Solution for task: 4. MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- create a test user and test database on localhost
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd' ;
+
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db` ;
+
+GRANT ALL PRIVILEDGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION ;
+
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION ;
+
+FLUSH PRIVILEDGES ;
