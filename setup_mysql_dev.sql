a7d461fea954600652761f640819a49887f722f7
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-06-23 12:06:15 +0000

    correction for failing checks

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,11 @@
 -- this script prepares a MySQL server for the project
 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
-       IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 
-GRANT ALL PRIVILEDGES ON `hbnb_dev_db`.*
-      TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 
-GRANT SELECT ON `performance_schema`.*
-      TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEDGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEDGES;
